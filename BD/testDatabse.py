91ea4ac32bdb46b95ee0c63b9dd1973cd5374a4a
Isadora <izadora.bezie@gmail.com>
2025-01-15 15:02:31 +0100

    test pythons mis à jour

--- BD/testDatabse.py
+++ BD/testDatabse.py
@@ -16,8 +16,8 @@ with engine.connect() as connection:
         try:
             connection.execute(
                 text("""INSERT INTO T_COMPETITION_CMP (CMP_ADDRESS, CMP_DATE_BEGIN, CMP_DATE_END, CMP_NAME, CMP_DESC) 
-                    VALUES ('123 Main St', '2025-01-01', '2025-01-10', 'Winter Championship', 'Annual winter event'),
-                    ('456 Elm St', '2025-02-01', '2025-02-10', 'Spring Championship', 'Annual spring event');"""),
+                    VALUES ('TEST 123 Main St', '2000-01-01', '2000-01-20', 'TESTWinter Championship', 'TEST Annual winter event'),
+                    ('TEST 456 Elm St', '2000-02-01', '2000-02-10', 'TESTSpring Championship', 'TEST Annual spring event');"""),
             )
             print("Insertion successful into T_COMPETITION_CMP")
         except Exception as e:
@@ -27,7 +27,7 @@ with engine.connect() as connection:
         try:
             connection.execute(
                 text("""INSERT INTO T_COMPETITION_CMP (CMP_ADDRESS, CMP_DATE_BEGIN, CMP_DATE_END, CMP_NAME, CMP_DESC)
-                    VALUES ('789 Pine St', '2025-01-05', '2025-01-08', 'Overlap Test', 'This should fail');"""),
+                    VALUES ('TEST789 Pine St', '2025-01-05', '2025-01-08', 'TESTOverlap Test', 'TESTThis should fail');"""),
             )
             print("Insertion successful into T_COMPETITION_CMP")
         except Exception as e:
@@ -52,7 +52,7 @@ with engine.connect() as connection:
         # Inserting data into T_CHAMPIONSHIP_CHP
         try:
             connection.execute(text("""INSERT INTO T_CHAMPIONSHIP_CHP (CMP_ID)
-                                VALUES ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship'));""")
+                                VALUES ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship'));""")
             )
             print("Insertion successful into T_CHAMPIONSHIP_CHP")
         except Exception as e:
@@ -68,7 +68,7 @@ with engine.connect() as connection:
         
         # Inserting data into T_TOURNAMENT_TNM 
         try:
-            connection.execute(text("""INSERT INTO T_TOURNAMENT_TNM (CMP_ID) VALUES ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship'));""")
+            connection.execute(text("""INSERT INTO T_TOURNAMENT_TNM (CMP_ID) VALUES ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship'));""")
             )
             print("Insertion successful into T_TOURNAMENT_TNM")
         except Exception as e:
@@ -87,9 +87,9 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """ INSERT INTO T_USER_USR (USR_TYPE, USR_MAIL, USR_PASS, USR_CREATION_DATE)
-                    VALUES ('ADMIN', 'admin@example.com', 'securepassword', '2025-01-01'),
-                    ('USER', 'user1@example.com', 'password123', '2025-01-02'),
-                    ('USER', 'user2@example.com', 'password123', '2025-01-02');
+                    VALUES ('ADMIN', 'TESTadmin@example.com', 'TESTsecurepassword', '2025-01-01'),
+                    ('USER', 'TESTuser1@example.com', 'TESTpassword123', '2025-01-02'),
+                    ('USER', 'TESTuser2@example.com', 'TESTpassword123', '2025-01-02');
                 """),
             )
             print("Insertion successful into T_USER_USR")
@@ -110,7 +110,7 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """ INSERT INTO T_USER_USR (USR_TYPE, USR_MAIL, USR_PASS, USR_CREATION_DATE)
-                    VALUES ('USER', 'user2@example.com', 'test123', '2025-01-03')""")
+                    VALUES ('USER', 'TESTuser2@example.com', 'TESTtest123', '2025-01-03')""")
             )
             print("Insertion successful into T_USER_USR")
         except Exception as e:
@@ -135,8 +135,8 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """ INSERT INTO T_TEAM_TEM (USR_ID, TEM_NAME, TEM_CREATION_DATE, CMP_ID)
-                    VALUES ((select USR_ID from T_USER_USR where USR_MAIL like 'user1@example.com'), 'Blue Warriors', '2025-01-05', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship')),
-                    ((select USR_ID from T_USER_USR where USR_MAIL like 'user2@example.com'), 'Green Giants', '2025-01-06', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Winter Championship'));""")
+                    VALUES ((select USR_ID from T_USER_USR where USR_MAIL like 'TESTuser1@example.com'), 'TESTBlue Warriors', '2025-01-05', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship')),
+                    ((select USR_ID from T_USER_USR where USR_MAIL like 'TESTuser2@example.com'), 'TESTGreen Giants', '2025-01-06', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTWinter Championship'));""")
             )
             print("Insertion successful into T_TEAM_TEM")
         except Exception as e:
@@ -146,7 +146,7 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """ INSERT INTO T_TEAM_TEM (USR_ID, TEM_NAME, TEM_CREATION_DATE, CMP_ID)
-                    VALUES ((select USR_ID from T_USER_USR  where USR_MAIL like 'user1@example.com'), 'Grey Things', '2025-01-07', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship'));
+                    VALUES ((select USR_ID from T_USER_USR  where USR_MAIL like 'TESTuser1@example.com'), 'TESTGrey Things', '2025-01-07', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship'));
                     """)
             )
             print("Insertion successful into T_TEAM_TEM")
@@ -157,7 +157,7 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """ INSERT INTO T_TEAM_TEM (USR_ID, TEM_NAME, TEM_CREATION_DATE, CMP_ID)
-                    VALUES ((select USR_ID from T_USER_USR  where USR_MAIL like 'user2@example.com'), 'Blue Warriors', '2025-01-07', 1);
+                    VALUES ((select USR_ID from T_USER_USR  where USR_MAIL like 'TESTuser2@example.com'), 'TESTBlue Warriors', '2025-01-07', (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship'));
                     """)
             )
             print("Insertion successful into T_TEAM_TEM")
@@ -177,8 +177,8 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """ INSERT INTO T_MEMBER_MBR (TEM_ID, MBR_NAME, MBR_SURNAME, MBR_MAIL)
-                    VALUES ((select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Blue Warriors'), 'John', 'Doe', 'john.doe@example.com'),  
-                    ((select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Green Giants'), 'Jane', 'Smith', 'jane.smith@example.com');
+                    VALUES ((select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTBlue Warriors'), 'John', 'Doe', 'TESTjohn.doe@example.com'),  
+                    ((select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTGreen Giants'), 'Jane', 'Smith', 'TESTjane.smith@example.com');
                     """)
             )
             print("Insertion successful into T_MEMBER_MBR")
@@ -202,7 +202,7 @@ with engine.connect() as connection:
         # Inserting data into T_FIELD_FLD
         try:
             connection.execute(
-                text("INSERT INTO T_FIELD_FLD (CMP_ID, FLD_NAME) VALUES ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship'), 'Field A'), ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship'), 'Field B');")
+                text("INSERT INTO T_FIELD_FLD (CMP_ID, FLD_NAME) VALUES ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship'), 'TESTField A'), ((select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship'), 'TESTField B');")
             )
             print("Insertion successful into T_FIELD_FLD")
         except Exception as e:
@@ -223,18 +223,46 @@ with engine.connect() as connection:
             print("No duplicates in field names.")
         print(f"Total fields: {len(df_field)}")
 
+    # Inserting data into T_TIMESLOT_SLT
+        try:
+            connection.execute(text(
+                """
+                INSERT INTO T_TIMESLOT_SLT (SLT_DATE_BEGIN, SLT_DATE_END)
+                VALUES ('2000-01-07 10:00:00', '2000-01-07 12:00:00'),
+                ('2000-01-07 11:00:00', '2000-01-07 13:00:00'),
+                ('2000-01-07 09:00:00', '2000-01-07 11:00:00');
+                """)
+            )
+            print("Insertion successful into T_TIMESLOT_SLT")
+        except Exception as e:
+            print(f"Error inserting into T_TIMESLOT_SLT: {e}")
+
+
+        try:
+            df_user = pd.read_sql("SELECT * FROM T_TIMESLOT_SLT", connection)
+            print("Data from T_TIMESLOT_SLT:")
+            print(df_user)
+        except Exception as e:
+                print(f"Error reading from T_TIMESLOT_SLT: {e}")
+
+    # Integrity check: ensure beginning date is before end date    
+        if (df_user['SLT_DATE_BEGIN'] > df_user['SLT_DATE_BEGIN']).all():
+            print("Integrity error: beginning date is after end date.")
+        else:
+            print("Beginning date is always before end date.")
 
     # Inserting data into T_ENCOUNTER_ENC
         try:
             connection.execute(text(
                 """
-                INSERT INTO T_ENCOUNTER_ENC (TNM_ID, CHP_ID, FLD_ID, TEM_ID_BLUE, TEM_ID_GREEN, ENC_STATE, ENC_DATE_BEGIN, ENC_DATE_END, ENC_SCORE_BLUE, ENC_SCORE_GREEN)
+                INSERT INTO T_ENCOUNTER_ENC (TNM_ID, CHP_ID, FLD_ID, TEM_ID_BLUE, TEM_ID_GREEN, ENC_STATE, SLT_ID, ENC_SCORE_BLUE, ENC_SCORE_GREEN, ENC_PENALTY_BLUE, ENC_PENALTY_GREEN)
                 VALUES (NULL, 
-                (select CHP_ID from T_CHAMPIONSHIP_CHP where CMP_ID = (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship')),
-                (select FLD_ID from T_FIELD_FLD where FLD_NAME like 'Field A'),
-                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Blue Warriors'),
-                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Green Giants'),
-                'CONCLUE', '2025-01-07 10:00:00', '2025-01-07 12:00:00', 3, 1);
+                (select CHP_ID from T_CHAMPIONSHIP_CHP where CMP_ID = (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship')),
+                (select FLD_ID from T_FIELD_FLD where FLD_NAME like 'TESTField A'),
+                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTBlue Warriors'),
+                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTGreen Giants'),
+                'CONCLUE', (select SLT_ID from T_TIMESLOT_SLT where SLT_DATE_BEGIN = '2000-01-07 10:00:00')
+                , 3, 1, FALSE, FALSE);
                 """)
             )
             print("Insertion successful into T_ENCOUNTER_ENC")
@@ -246,16 +274,17 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """
-                INSERT INTO T_ENCOUNTER_ENC (TNM_ID, CHP_ID, FLD_ID, TEM_ID_BLUE, TEM_ID_GREEN, ENC_STATE, ENC_DATE_BEGIN, ENC_DATE_END, ENC_SCORE_BLUE, ENC_SCORE_GREEN)
+                INSERT INTO T_ENCOUNTER_ENC (TNM_ID, CHP_ID, FLD_ID, TEM_ID_BLUE, TEM_ID_GREEN, ENC_STATE, SLT_ID, ENC_SCORE_BLUE, ENC_SCORE_GREEN, ENC_PENALTY_BLUE, ENC_PENALTY_GREEN)
                 VALUES (NULL, 
-                (select CHP_ID from T_CHAMPIONSHIP_CHP where CMP_ID = (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship')),
-                (select FLD_ID from T_FIELD_FLD where FLD_NAME like 'Field B'),
-                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Blue Warriors'),
-                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Green Giants'),
-                'CONCLUE', '2025-01-07 11:00:00', '2025-01-07 13:00:00', 2, 1);
+                (select CHP_ID from T_CHAMPIONSHIP_CHP where CMP_ID = (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship')),
+                (select FLD_ID from T_FIELD_FLD where FLD_NAME like 'TESTField B'),
+                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTBlue Warriors'),
+                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTGreen Giants'),
+                'CONCLUE', (select SLT_ID from T_TIMESLOT_SLT where SLT_DATE_BEGIN = '2000-01-07 11:00:00')
+                , 2, 1, FALSE, FALSE);
             """)
             )
-            print("Insertion successful into T_ENCOUNTER_ENC")
+            print("FAIL : Insertion successful into T_ENCOUNTER_ENC Duplicate teams with overlapping dates")
         except Exception as e:
             print(f"TEST : Error inserting into T_ENCOUNTER_ENC: Duplicate teams with overlapping dates")
 
@@ -263,16 +292,16 @@ with engine.connect() as connection:
         try:
             connection.execute(text(
                 """
-                INSERT INTO T_ENCOUNTER_ENC (TNM_ID, CHP_ID, FLD_ID, TEM_ID_BLUE, TEM_ID_GREEN, ENC_STATE, ENC_DATE_BEGIN, ENC_DATE_END, ENC_SCORE_BLUE, ENC_SCORE_GREEN)
+                INSERT INTO T_ENCOUNTER_ENC (TNM_ID, CHP_ID, FLD_ID, TEM_ID_BLUE, TEM_ID_GREEN, ENC_STATE, SLT_ID, ENC_SCORE_BLUE, ENC_SCORE_GREEN, ENC_PENALTY_BLUE, ENC_PENALTY_GREEN)
                 VALUES (NULL, 
-                (select CHP_ID from T_CHAMPIONSHIP_CHP where CMP_ID = (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'Spring Championship')),
-                (select FLD_ID from T_FIELD_FLD where FLD_NAME like 'Field A'),
-                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Blue Warriors'),
-                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'Green Giants'), 
-                'PROGRAMMEE', '2025-01-07 09:00:00', '2025-01-07 11:00:00', NULL, NULL);
+                (select CHP_ID from T_CHAMPIONSHIP_CHP where CMP_ID = (select CMP_ID from T_COMPETITION_CMP where CMP_NAME like 'TESTSpring Championship')),
+                (select FLD_ID from T_FIELD_FLD where FLD_NAME like 'TESTField A'),
+                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTBlue Warriors'),
+                (select TEM_ID from T_TEAM_TEM where TEM_NAME like 'TESTGreen Giants'), 
+                'PROGRAMMEE', (select SLT_ID from T_TIMESLOT_SLT where SLT_DATE_BEGIN = '2000-01-07 09:00:00'), NULL, NULL, FALSE, FALSE);
                 """)
             )
-            print("Insertion successful into T_ENCOUNTER_ENC")
+            print("FAIL : Insertion successful into T_ENCOUNTER_ENC Duplicate field with overlapping dates")
         except Exception as e:
             print(f"TEST : Error inserting into T_ENCOUNTER_ENC: Duplicate field with overlapping dates")
 
@@ -284,12 +313,7 @@ with engine.connect() as connection:
         except Exception as e:
             print(f"Error reading from T_ENCOUNTER_ENC: {e}")
 
-        # Integrity check: ensure beginning date is before end date
-        if (df_user['ENC_DATE_BEGIN'] > df_user['ENC_DATE_END']).all():
-            print("Integrity error: beginning date is after end date.")
-        else:
-            print("Beginning date is always before end date.")
-        
+
         # Integrity check: ensure blue team is different from green team
         if df_user['TEM_ID_BLUE'].equals(df_user['TEM_ID_GREEN']):
             print("Integrity error: blue team is the same as green team.")
